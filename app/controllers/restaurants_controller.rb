b809311e8904b347a52d4a7a23af4a50914ceaf8
FengYue <10761101@gm.nfu.edu.tw>
2021-07-16 17:29:51 +0800

    true false 判斷是否有登入 有登入顯示、無登入消失

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,8 +2,8 @@ class RestaurantsController < ApplicationController
   # repo:https://stackoverflow.com/questions/3364492/actioncontrollerinvalidauthenticitytoken
   # skip_before_action :verify_authenticity_token
 
-  def index
-  end
+ def index
+ end
 
  def new
  end
