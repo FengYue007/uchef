1b96bd87c6d7a0ffafbea7fa48a8912fe3a119c3
FengYue <10761101@gm.nfu.edu.tw>
2021-07-16 17:35:38 +0800

    更改專屬路徑

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/restaurants', to: 'restaurants#index'
-  get '/new_restaurant', to: 'restaurants#new'
+
+  get :new_restaurant, to: 'restaurants#new', path: 'ccc'
+
   post '/restaurant_list', to: 'restaurants#list'
 end
