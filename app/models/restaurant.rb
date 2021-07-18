4a2d10185ff8b0297942041ca9b05f5822c73ac6
FengYue <10761101@gm.nfu.edu.tw>
2021-07-18 21:34:25 +0800

    model 驗證寫法

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,11 @@
 class Restaurant < ApplicationRecord
-  validates :title, presence: true
+  # validates( :title, presence: true )
+  validates :restaurant, presence: true
+
+  validates :restaurant, :title, :email, presence: true
+  # validates :restaurant, :email, presence: true
+  
+  # 建議寫兩行！
+  validates :restaurant, :title, presence: true
+  validates :restaurant, :email, presence: true
 end
