415831b70e0d32cb3d5e4df00395464aad38ddff
FengYue <10761101@gm.nfu.edu.tw>
2021-07-18 20:56:20 +0800

    查看rails 清洗資料，使用debugger

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,12 +11,17 @@ class RestaurantsController < ApplicationController
 
  def create
    # 寫入資料庫
+   r = params.require(:restaurant).permit(:title, :tel, :address, :email, :description) 
 
-  render html:params
-   
-   @restaurant = Restaurant.new
+   debugger # 豬八戒
 
-   
+   # @restaurant = Restaurant.new(params[:restaurant])
+   #
+   # if @restaurant.save
+   #   redirect_to '/restaurants'
+   # else
+   #   # .....
+   # end
    # redirect -> 列表頁
    # render html: "Title is #{params[:title]}
    # render html: "Title is #{params[:title]}、Tel is #{params[:tel]}、Address #{params[:address]}、Description is #{params[:description]}"
