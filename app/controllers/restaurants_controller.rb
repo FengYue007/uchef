f06435aaa7b453e6577c834fcbffdf62294cd6e0
FengYue <10761101@gm.nfu.edu.tw>
2021-07-19 18:18:48 +0800

    如果id有找不到 在最上方寫入recuse_from....

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,6 @@
 class RestaurantsController < ApplicationController
-  # repo:https://stackoverflow.com/questions/3364492/actioncontrollerinvalidauthenticitytoken
-  # skip_before_action :verify_authenticity_token
+
+rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
  def index
    @restaurants = Restaurant.order(id: :desc)
@@ -8,11 +8,9 @@ class RestaurantsController < ApplicationController
  end
 
  def show
-   @restaurant = Restaurant.find(params[:id]) # 只能找id
-   # Exception 例外
-   # Error 錯誤
- rescue
-   redirect_to restaurants_path
+     @restaurant = Restaurant.find(params[:id]) # 只能找id
+     # Exception 例外
+     # Error 錯誤
  end
 
  def new
@@ -40,5 +38,9 @@ class RestaurantsController < ApplicationController
    def restaurant_params
      params.require(:restaurant).permit(:title, :tel, :address, :email, :description)
      # params[:restaurant].permit(:title, :tel, :address, :email, :description)
+   end
+
+   def not_found
+     redirect_to restaurants_path
    end
 end
