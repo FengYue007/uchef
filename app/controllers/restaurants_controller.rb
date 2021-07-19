8e501f8c67210f798eddd25da95dccb9038c945c
FengYue <10761101@gm.nfu.edu.tw>
2021-07-19 18:36:34 +0800

    每個controller都有可能找不到id的狀態，用繼承的根念，程式碼放在Application_Controller裡面

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,4 @@
 class RestaurantsController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
  def index
    @restaurants = Restaurant.order(id: :desc)
@@ -26,10 +25,5 @@ class RestaurantsController < ApplicationController
  private
    def restaurant_params
      params.require(:restaurant).permit(:title, :tel, :address, :email, :description)
-   end
-
-   def not_found
-     # redirect_to restaurants_path
-     render file: "#{Rails.root}/public/404.html", status: 404
    end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+   def not_found
+     render file: "#{Rails.root}/public/404.html",
+            status: 404
+   end
 end
