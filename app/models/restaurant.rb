56f9182a3744c14df57b8f880d0f5942d53589c9
FengYue <10761101@gm.nfu.edu.tw>
2021-07-23 18:12:37 +0800

    在餐廳model 寫入真刪除方法

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,14 +2,20 @@ class Restaurant < ApplicationRecord
   validates :title, presence: true
 
   default_scope { available }
-  scope :available, -> { where(deleted_at: nil).order(id: :desc) }
+  scope :available, -> { where(deleted_at: nil) }
 
   # Restaurant.deleted
   def self.deleted
     unscope(:where).where.not(deleted_at:nil)
   end
 
+  # 給使用者用的
   def destroy
     update(deleted_at: Time.now)
+  end
+
+  # 給管理人員用的
+  def really_destroy!
+    super.destroy
   end
 end
