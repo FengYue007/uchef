42e1f9c94e9e6ec266f37dfed2feb59c0698357f
FengYue <10761101@gm.nfu.edu.tw>
2021-07-23 17:39:01 +0800

    寫一個類別方法，可以在Rails c 查詢已刪除的餐廳

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,6 +4,11 @@ class Restaurant < ApplicationRecord
   default_scope { available }
   scope :available, -> { where(deleted_at: nil).order(id: :desc) }
 
+  # Restaurant.deleted
+  def self.deleted
+    unscope(:where).where.not(deleted_at:nil)
+  end
+
   def destroy
     update(deleted_at: Time.now)
   end
