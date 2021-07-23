2f07ec16901a8c8e974d94ba4ba5f13156d0ac22
FengYue <10761101@gm.nfu.edu.tw>
2021-07-23 14:42:40 +0800

    增加migration跟索引

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_17_121802) do
+ActiveRecord::Schema.define(version: 2021_07_23_053134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2021_07_17_121802) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_restaurants_on_deleted_at"
   end
 
 end
