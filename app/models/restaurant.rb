bdcd39a17103006e8269402df1aa71c2b268ef4e
FengYue <10761101@gm.nfu.edu.tw>
2021-07-23 18:55:16 +0800

    在餐廳model 使用paranoid 方法

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,21 +1,5 @@
 class Restaurant < ApplicationRecord
+  act_as_paranoid
   validates :title, presence: true
-
-  default_scope { available }
-  scope :available, -> { where(deleted_at: nil) }
-
-  # Restaurant.deleted
-  def self.deleted
-    unscope(:where).where.not(deleted_at:nil)
-  end
-
-  # 給使用者用的
-  def destroy
-    update(deleted_at: Time.now)
-  end
-
-  # 給管理人員用的
-  def really_destroy!
-    super.destroy
-  end
+  
 end
