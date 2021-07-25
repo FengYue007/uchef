9cae25a5291559a73696bfb51509a990f93dd2fe
FengYue <10761101@gm.nfu.edu.tw>
2021-07-25 19:21:36 +0800

    輸入密碼 SHA1加密

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,18 @@ class User < ApplicationRecord
                     format: { with: /.+\@.+\..+/ }
   validates :password, presence: true,
                        confirmation: true
+
+  before_create :encrypt_password
+
+  private
+  def encrypt_password
+    # 加密
+    require 'digest'
+    self.password = Digest::SHA1.hexdigest(self.password) 
+    # self.password = Digest::SHA1.hexdigest(password)
+    # self.password = (Digest::SHA1.hexdigest(password))
+    # password = Digest::SHA1.hexdigest(password)
+  end
+
+
 end
