51d73a2bea3164417037ba9d8b3144614b7de665
FengYue <10761101@gm.nfu.edu.tw>
2021-07-25 19:47:22 +0800

    WIP - user password encrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,14 @@ class User < ApplicationRecord
   def encrypt_password
     # 加密
     require 'digest'
-    self.password = Digest::SHA1.hexdigest(self.password) 
+    self.password = Digest::SHA1.hexdigest(salted_pwd) 
     # self.password = Digest::SHA1.hexdigest(password)
     # self.password = (Digest::SHA1.hexdigest(password))
     # password = Digest::SHA1.hexdigest(password)
   end
 
+  def salted_pwd
+    "123#{self.password}123"
+  end
 
 end
