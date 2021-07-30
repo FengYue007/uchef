c85cb5b4c9214895c8a0c5607b1fa3829666682b
FengYue <10761101@gm.nfu.edu.tw>
2021-07-31 01:07:56 +0800

    add authorize for restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-  before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
+  before_action :find_restaurant, only: [:edit, :update, :destroy]
   # before_action :find_restaurant, except: [:index, :new, :create]
   before_action :check_user!, except: [:index, :show]
 
@@ -8,7 +8,7 @@ class RestaurantsController < ApplicationController
  end
 
  def show
-   # @restaurant = Restaurant.find(params[:id]) # 只能找id
+   @restaurant = Restaurant.find(params[:id]) 
  end
 
  def new
@@ -16,7 +16,12 @@ class RestaurantsController < ApplicationController
  end
 
  def create
-   @restaurant = Restaurant.new(restaurant_params)
+   # @restaurant = Restaurant.new(restaurant_params)
+   # @restaurant.user_id = current_user.id
+
+   # @restaurant = Restaurant.new(restaurant_params)
+   # @restaurant.user = current_user
+   @restaurant = current_user.restaurants.new(restaurant_params)
    
    if @restaurant.save
      redirect_to restaurants_path
@@ -46,8 +51,17 @@ class RestaurantsController < ApplicationController
  end
 
  private
+   # authentication 指允許可以進來這個大門，代表有認證。 但不能進去。 (驗證)
+   # authorization 有沒有授權，可不可以做這件事情。                   (授權)
+   # 1
    def find_restaurant
-     @restaurant = Restaurant.find(params[:id]) 
+     # @restaurant = Restaurant.find(params[:id]) 
+     # @restaurant = Restaurant.find_by!(
+     #   id: params[:id],
+     #   user_id: current_user:id
+     # )
+   # 2
+     @restaurant = current_user.restaurants.find(params[:id])
    end
 
    def restaurant_params
